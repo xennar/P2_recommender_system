78ad85db0ee2d5f85c6810bc8e14d679340b3c1f
Mads <madhan16@student.aau.dk>
2018-04-19 10:20:04 +0200

    formatted
    fjernede nogle unødvendige kommentare

--- Movie_Recommender/src/InfoGatherer.java
+++ Movie_Recommender/src/InfoGatherer.java
@@ -1,15 +1,12 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
-//package p2;
 public class InfoGatherer {
 
     public static void MakeRatings() throws Exception {
-        
+
         File RatingsFile = new File("Movie_Recommender\\src\\ratings.csv");
         Scanner scan = new Scanner(RatingsFile);
         FileWriter fileWriter = new FileWriter("adjratings.csv");
--- Movie_Recommender/src/Main.java
+++ Movie_Recommender/src/Main.java
@@ -1,4 +1,3 @@
-//import package p2;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("active");
