d46c2f04cf648327dd00abfcac77b708911d2394
esindb17 <34280802+esindb17@users.noreply.github.com>
2018-04-19 10:15:34 +0200

    sådan der virker

--- Movie_Recommender/src/InfoGatherer.java
+++ Movie_Recommender/src/InfoGatherer.java
@@ -4,24 +4,24 @@ import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
 //package p2;
 public class InfoGatherer {
 
     public static void MakeRatings() throws Exception {
+        
+        File RatingsFile = new File("Movie_Recommender\\src\\ratings.csv");
+        Scanner scan = new Scanner(RatingsFile);
+        FileWriter fileWriter = new FileWriter("adjratings.csv");
+        PrintWriter writer = new PrintWriter(fileWriter);
 
-            //Path filePath = new Path.get("ratings.csv");
-            File RatingsFile = new File("ratings.csv");
-            Scanner scan = new Scanner(RatingsFile);
-            FileWriter fileWriter = new FileWriter("adjratings.csv");
-            PrintWriter writer = new PrintWriter(fileWriter);
-
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] adjRatings = line.split(",");
-                writer.printf(adjRatings[0] + ", " + adjRatings[1] + ", " + adjRatings[2] + "\n");
-            }
-            
-
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] adjRatings = line.split(",");
+            writer.printf(adjRatings[0] + ", " + adjRatings[1] + ", " + adjRatings[2] + "\n");
         }
+
+
     }
+}
 
