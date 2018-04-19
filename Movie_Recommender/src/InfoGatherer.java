e80ab7aff9d017a40ac28c35818f6f43f289241a
esindb17 <34280802+esindb17@users.noreply.github.com>
2018-04-19 10:03:32 +0200

    firstgo

--- Movie_Recommender/src/InfoGatherer.java
+++ Movie_Recommender/src/InfoGatherer.java
@@ -1,2 +1,27 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+//package p2;
 public class InfoGatherer {
-}
+
+    public static void MakeRatings() throws Exception {
+
+            //Path filePath = new Path.get("ratings.csv");
+            File RatingsFile = new File("ratings.csv");
+            Scanner scan = new Scanner(RatingsFile);
+            FileWriter fileWriter = new FileWriter("adjratings.csv");
+            PrintWriter writer = new PrintWriter(fileWriter);
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] adjRatings = line.split(",");
+                writer.printf(adjRatings[0] + ", " + adjRatings[1] + ", " + adjRatings[2] + "\n");
+            }
+            
+
+        }
+    }
+
---
+++ Movie_Recommender/src/Main.java
@@ -0,0 +1,8 @@
+//import package p2;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("active");
+        InfoGatherer ratingsMake = new InfoGatherer();
+        ratingsMake.MakeRatings();
+    }
+}
