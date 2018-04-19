904b780a975ec666e4ee86481f6e07016ee65bc3
esindb17 <34280802+esindb17@users.noreply.github.com>
2018-04-19 14:24:43 +0200

    almost working

--- Movie_Recommender/src/Reader.java
+++ Movie_Recommender/src/Reader.java
@@ -10,26 +10,17 @@ public class Reader {
         File movies = new File("Movie_Recommender\\src\\movies.csv");
         Scanner scanMovies = new Scanner(movies);
 
-
-        //String[] moviesTemp = null;
-        ArrayList<String[]> movieTempList = new ArrayList<>();
         while (scanMovies.hasNextLine()) {
             String tempLine = scanMovies.nextLine();
             String[] temp = tempLine.split(",");
             if (temp.length > 3) {
                 String combineMovie = temp[1] + "," + temp[2];
                 String[] newtemp = new String[]{temp[0], combineMovie, temp[3]};
-                System.out.println(newtemp[0] + newtemp[1] + newtemp[2]);
-                movieTempList.add(newtemp);
-            } else
-                movieTempList.add(temp);
-            //for(String[] item : movieTempList) {
-            //  int i = 0;
-            //while (i < item.length){
-            //System.out.println(item[i]);
-            //i++;
+                //add newtemp[0] to productID, add newtemp[1] to name, add newtemp[2] to list<strings> genre
+            } //else
+            //add temp[0] to productID, add temp[1] to name, add temp[2] to list<strings> genre
+
         }
     }
-
 }
 
