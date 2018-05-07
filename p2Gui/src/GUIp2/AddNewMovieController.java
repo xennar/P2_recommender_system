60a136efd445fb75f360ffaaf2281b9c56387e79
esindb17 <34280802+esindb17@users.noreply.github.com>
2018-05-07 07:40:59 +0200

    Add files via upload

---
+++ p2Gui/src/GUIp2/AddNewMovieController.java
@@ -0,0 +1,13 @@
+package GUIp2;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddNewMovieController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ p2Gui/src/GUIp2/PreviousRatingsController.java
@@ -0,0 +1,17 @@
+package GUIp2;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PreviousRatingsController implements Initializable {
+    @FXML
+    AnchorPane PreviousRaintgsScreen;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
